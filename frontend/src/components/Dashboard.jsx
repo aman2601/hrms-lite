8792321b2d36ac51b49f12e5085c0953ef094716
Aman Shahil <66594118+aman2601@users.noreply.github.com>
2026-02-18 22:51:59 +0530

    Update API endpoint to production URL

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://hrms-lite-dvkp.onrender.com";
 
 export default function Dashboard() {
   const [count, setCount] = useState(0);
